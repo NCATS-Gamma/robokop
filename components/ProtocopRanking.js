bea8ca282b81618e86bff5ed9e044f5ac949f59c
Kenneth Morton <kennethmorton@gmail.com>
2017-11-22 12:03:37 -0500

    Added a warning message prior to ranking large graphs.

--- components/ProtocopRanking.js
+++ components/ProtocopRanking.js
@@ -102,15 +102,25 @@ class ProtocopRanking extends React.Component {
     // const isSummary = !(graph == null) && Object.prototype.hasOwnProperty.call(graph, 'node_count');
     const isGraph = !(graph == null) && (Object.prototype.hasOwnProperty.call(graph, 'nodes'));
     let isEmpty = false;
+
+    let nNodes = 0;
+    let nEdges = 0;
     if (isGraph) {
       isEmpty = graph.nodes.length === 0;
+      nNodes = graph.nodes.length;
+      nEdges = graph.edges.length;
     } else { // isSummary
       isEmpty = graph.node_count === 0;
+      nNodes = graph.node_count;
+      nEdges = graph.edge_count;
     }
+
+    const isTooBig = nNodes > 200 || nEdges > 6000;
 
     const showNoAnswers = noAnswers;
     const showEmptyGraph = !noAnswers && isEmpty;
-    const showEvalButton = !noAnswers && !isEmpty && noRank;
+    const showTooBigEvalButton = !noAnswers && !isEmpty && isTooBig;
+    const showEvalButton = !noAnswers && !isEmpty && noRank && !isTooBig;
     const showAnswers = !noAnswers && !isEmpty && !noRank;
 
     return (
@@ -131,6 +141,26 @@ class ProtocopRanking extends React.Component {
               <div className="col-md-6 col-md-offset-3">
                 <h2>{'This is an empty graph.'}</h2>
                 <p>{'We can\'t provide answers without any input. Try another blackboard.'}</p>
+              </div>
+            </div>
+          </div>
+        }
+        {showTooBigEvalButton &&
+          <div id="ProtocopRanking_NoRank" className="col-md-12">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3">
+                <h2>We still need to evaluate your blackboard.</h2>
+                <p>After your blackboard is created we need to analyze it to determine and rank answers to your query.</p>
+                <h3>Warning: This is a really big graph. It might be a bit too large for PROTOCOP to return answers in a timely manner.</h3>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-2 col-md-offset-5" style={{ paddingTop: '10px' }}>
+                <Button bsStyle="default" bsSize="sm" onClick={this.props.callbacks.blackboardRank}>
+                  Get Answers
+                  <br />
+                  <Glyphicon glyph="random" />
+                </Button>
               </div>
             </div>
           </div>
