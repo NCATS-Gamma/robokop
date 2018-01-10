96243b2ed7133e5c221939d426c48febcbc4d9f5
Patrick Wang <patrick@covar.com>
2018-01-10 12:50:19 -0800

    switch to default port 80

--- serverConfig.py
+++ serverConfig.py
@@ -7,7 +7,7 @@ import json
 
 # Our local config is in the main directory along with this config.
 # We will use this host and port if we are running from python and not gunicorn
-local_config = {'serverHost': '0.0.0.0', 'port': 5000} # Defaults
+local_config = {'serverHost': '0.0.0.0', 'port': 80} # Defaults
 
 config_dir = os.path.dirname(os.path.realpath(__file__))
 json_file = os.path.join(config_dir,'config.json')
