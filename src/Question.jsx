fca3b8ccfcc84cd2f5c90bbfda5e5f97d1c320a6
Kenneth Morton <kennethmorton@gmail.com>
2018-02-22 17:04:38 -0500

    Question react component mods.

--- src/Question.jsx
+++ src/Question.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import AppConfig from './AppConfig';
 import Loading from './components/Loading';
 import Header from './components/Header';
-import Loading from './components/Loading';
 
+import customPropTypes from './customPropTypes';
 
 class Question extends React.Component {
   constructor(props) {
@@ -16,6 +17,7 @@ class Question extends React.Component {
       ready: false,
       timestamp: null,
       user: {},
+      question: {},
     };
   }
 
@@ -23,6 +25,7 @@ class Question extends React.Component {
     this.appConfig.questionData(this.props.id, data => this.setState({
       timestamp: data.timestamp,
       user: data.user,
+      question: data.question,
       ready: true,
     }));
   }
@@ -39,8 +42,11 @@ class Question extends React.Component {
           config={this.props.config}
           user={this.state.user}
         />
-        <h1>{'Question'}</h1>
-        <h3>{`Time: ${this.state.timestamp}`}</h3>
+        <h1>Question:</h1>
+        <p>
+          {JSON.stringify(this.state.question)}
+        </p>
+        <h5>{`Time: ${this.state.timestamp}`}</h5>
       </div>
     );
   }
@@ -53,5 +59,10 @@ class Question extends React.Component {
     );
   }
 }
+
+Question.propTypes = {
+  config: customPropTypes.config.isRequired,
+  id: PropTypes.string.isRequired,
+};
 
 export default Question;
