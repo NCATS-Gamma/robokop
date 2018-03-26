9848453360e0a56e5ba57e354f73919f92c44a29
Patrick Wang <patrick@covar.com>
2018-03-26 16:32:41 -0400

    set up Neo4j plugin loading

--- start_all.sh
+++ start_all.sh
@@ -22,6 +22,11 @@ docker network create \
     --subnet=172.18.0.0/16 \
     robokop-docker-net
 
+# Neo4J plugins
+mkdir ./neo4j_plugins
+curl https://github.com/neo4j-contrib/neo4j-apoc-procedures/releases/download/3.3.0.2/apoc-3.3.0.2-all.jar -o neo4j_plugins/apoc.jar
+curl https://github.com/NCATS-Gamma/robokop-neo4j-plugin/releases/download/v1.0.0/robokop-1.0.0.jar -o neo4j_plugins/robokop.jar
+
 # Neo4j:
 # https://hub.docker.com/_/neo4j/
 # https://neo4j.com/docs/operations-manual/current/installation/docker/
@@ -32,8 +37,10 @@ docker run \
     --ip $NEO4J_HOST \
     --env NEO4J_dbms_security_auth__enabled=false \
     --env NEO4J_dbms_connectors_default__listen__address=0.0.0.0 \
+    --publish 7474:7474 \
+    -v $(pwd)/neo4j_plugins:/var/lib/neo4j/plugins \
     -d \
-    neo4j
+    neo4j:3.3.4
 
 # Redis:
 # https://hub.docker.com/_/redis/
