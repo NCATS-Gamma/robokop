82910d6cffa47346367370695e1728eb92daed21
Patrick Wang <patrick@Patricks-MacBook-Pro.local>
2018-04-11 11:30:00 -0400

    stopping celery workers automatically when flower is stopped

--- workers_start.sh
+++ workers_start.sh
@@ -5,7 +5,9 @@
 cd $ROBOKOP_HOME/robokop/python
 celery multi start answerer@robokop updater@robokop -A tasks.celery -l info -c:1 4 -c:2 1 -Q:1 answer -Q:2 update
 celery flower -A tasks.celery
+# waits here for SIGINT
+celery multi stop answerer@robokop updater@robokop
 
-# equivalent to:
+# `celery multi start...`` is equivalent to:
 #   celery -A tasks.celery worker --loglevel=info -c 4 -n answerer@robokop -Q answer
 #   celery -A tasks.celery worker --loglevel=info -c 1 -n updater@robokop -Q update
