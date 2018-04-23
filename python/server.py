df5c2249db3011d52f0ed496556496e5944d1da3
patrickkwang <patrickelvin@gmail.com>
2018-04-23 15:59:20 -0400

    simplify question metadata

--- python/server.py
+++ python/server.py
@@ -237,24 +237,27 @@ def questions_data():
                   'refreshing KG' if t['name'] == 'tasks.update_kg' else
                   'initializing' for t in tasks]
 
-    question_status = [[task_types[i] for i in [j for j, h in enumerate(question_hashes) if h == q.hash]] for q in question_list]
-    user_question_status = [[task_types[i] for i in [j for j, h in enumerate(question_hashes) if h == q.hash]] for q in user_question_list]
-
-    def answerset_info(question_list):
-        latest_answerset_ids = []
-        latest_answerset_timestamps = []
-        for q in question_list:
-            answerset_timestamps = [a.timestamp for a in q.answersets]
-            latest_idx = answerset_timestamps.index(max(answerset_timestamps))
-            latest_answerset_ids.append(q.answersets[latest_idx].id)
-            latest_answerset_timestamps.append(q.answersets[latest_idx].timestamp)
-        return [{'latest_answerset_id':aid, 'latest_answerset_timestamp':ats} for aid, ats in zip(latest_answerset_ids, latest_answerset_timestamps)]
+    def augment_info(question):
+        answerset_timestamps = [a.timestamp for a in question.answersets]
+        latest_idx = answerset_timestamps.index(max(answerset_timestamps))
+        latest_answerset_id = question.answersets[latest_idx].id
+        latest_answerset_timestamp = question.answersets[latest_idx].timestamp
+        q = question.toJSON()
+        q.pop('user_id')
+        q.pop('nodes')
+        q.pop('edges')
+        tasks = [task_types[i] for i in [j for j, h in enumerate(question_hashes) if h == question.hash]]
+        return {
+            'latest_answerset_id':latest_answerset_id,
+            'latest_answerset_timestamp':latest_answerset_timestamp,
+            'tasks':tasks,
+            **q}
 
     now_str = datetime.now().__str__()
     return jsonify({'timestamp': now_str,\
-        'user': user,\
-        'questions': [{**q.toJSON(), **ainfo, 'tasks':s} for q, ainfo, s in zip(question_list, answerset_info(question_list), question_status)],\
-        'user_questions': [{**q.toJSON(), **ainfo, 'tasks':s} for q, ainfo, s in zip(user_question_list, answerset_info(user_question_list), user_question_status)]})
+                    'user': user,\
+                    'questions': [augment_info(q) for q in question_list],\
+                    'user_questions': [augment_info(q) for q in user_question_list]})
 
 # Question
 @app.route('/q/<question_id>', methods=['GET'])
