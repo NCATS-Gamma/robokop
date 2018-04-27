428b3ef7fd35268a41fa21d077c12de8f6ddc835
patrickkwang <patrickelvin@gmail.com>
2018-04-27 10:38:53 -0400

    separate manager and builder celery tasks

--- manager_workers_start.sh
+++ manager_workers_start.sh
@@ -3,10 +3,15 @@
 # This should only be run from the main robokop.git directory
 
 cd $ROBOKOP_HOME/robokop/python
-celery multi start answerer@robokop updater@robokop initializer@robokop -A tasks.celery -l info -c:1 4 -c:2 1 -c:3 4 -Q:1 answer -Q:2 update -Q:3 initialize
+celery multi start \
+    manager_answerer@robokop manager_updater@robokop manager_initializer@robokop \
+    -A tasks.celery \
+    -l info \
+    -c:1 4 -c:2 1 -c:3 4 \
+    -Q:1 manager_answer -Q:2 manager_update -Q:3 manager_initialize
 celery flower -A tasks.celery
 # waits here for SIGINT
-celery multi stop answerer@robokop updater@robokop initializer@robokop
+celery multi stop manager_answerer@robokop manager_updater@robokop manager_initializer@robokop
 
 # `celery multi start...`` is equivalent to:
 #   celery -A tasks.celery worker --loglevel=info -c 4 -n answerer@robokop -Q answer
