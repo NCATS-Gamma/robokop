111f42619b501044ed9b6ffa7d7a504cb4a724f5
Kenneth Morton <kennethmorton@gmail.com>
2018-06-20 14:43:04 -0400

    Cleaning up outdated files.

--- setup_terminal.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-# source $BASEDIR/shared/setenv.sh
-export $(cat $BASEDIR/shared/robokop.env | grep -v ^# | xargs)
-source activate robokop
