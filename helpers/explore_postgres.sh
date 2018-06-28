aec86b4dd535f2d98a1ba70436d54c7c91a960bd
patrickkwang <patrick@covar.com>
2018-06-28 09:54:26 -0400

    update psql script

--- helpers/explore_postgres.sh
+++ helpers/explore_postgres.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
-exec "docker run -it --rm --network=robokop_default postgres psql -h postgres -U $POSTGRES_USER $POSTGRES_DB"
+CMD="docker run -it --rm --network=robokop_default -e "PGPASSWORD=$POSTGRES_PASSWORD" postgres psql -h postgres -U $POSTGRES_USER $POSTGRES_DB"
+exec $CMD
