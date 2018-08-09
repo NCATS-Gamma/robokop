ccc54ca9f615d1354d3552b17989c0be751b8db3
Patrick Wang <patrick@covar.com>
2018-08-09 16:18:45 -0400

    one more

--- manager/user.py
+++ manager/user.py
@@ -2,21 +2,21 @@ from flask_security import UserMixin, RoleMixin
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Boolean, DateTime, Column, Integer, \
                        String, ForeignKey
-from manager.setup import db
+from manager.setup import Base
 
-class RolesUsers(db.Model):
+class RolesUsers(Base):
     __tablename__ = 'roles_users'
     id = Column(Integer, primary_key=True)
     user_id = Column('user_id', Integer, ForeignKey('user.id'))
     role_id = Column('role_id', Integer, ForeignKey('role.id'))
 
-class Role(db.Model, RoleMixin):
+class Role(Base, RoleMixin):
     __tablename__ = 'role'
     id = Column(Integer, primary_key=True)
     name = Column(String, unique=True)
     description = Column(String)
 
-class User(db.Model, UserMixin):
+class User(Base, UserMixin):
     __tablename__ = 'user'
     id = Column(Integer, primary_key=True)
     email = Column(String, unique=True)
