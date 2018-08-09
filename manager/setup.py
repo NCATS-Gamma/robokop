0098704a7347d23f208d89772350b46c99cbc4b6
Patrick Wang <patrick@covar.com>
2018-08-09 15:26:08 -0400

    traditional sqlalchemy setup

--- manager/setup.py
+++ manager/setup.py
@@ -1,6 +1,7 @@
 import os
 import json
 import sys
+from contextlib import contextmanager
 
 from flask import Flask, Blueprint
 from flask_mail import Mail
@@ -8,8 +9,11 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_restful import Api
 from flasgger import Swagger
 
-from sqlalchemy import Column, Integer, ForeignKey, Table, String
+from sqlalchemy import Column, Integer, ForeignKey, Table, String, create_engine
+from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 app = Flask(__name__, static_folder='../pack', template_folder='../templates')
 # Set default static folder to point to parent static folder where all
@@ -18,6 +22,23 @@ app.config.from_pyfile('robokop_flask_config.py')
 
 mail = Mail(app)
 db = SQLAlchemy(app)
+
+# traditional sqlalchemy setup, for celery tasks
+engine = create_engine(app.config['SQLALCHEMY_DATABASE_URI'])
+Base.metadata.bind = engine
+Session = sessionmaker(bind=engine)
+
+@contextmanager
+def session_scope():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
 
 api_blueprint = Blueprint('api', __name__, url_prefix='/api')
 api = Api(api_blueprint)
