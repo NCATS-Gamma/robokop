87ffb620a563538bf5bc7642ef6083bb8877bdbe
cbizon <bizon@renci.org>
2018-09-04 13:29:20 -0400

    1. Similarity Service 2. Converted pharos from smartAPI to mysql 3. Cleaned up naming in crawler for diseases/phenotypes.

--- manager/api/simple_api.py
+++ manager/api/simple_api.py
@@ -164,3 +164,144 @@ class Quick(Resource):
         return answerset_json.json()
 
 api.add_resource(Quick, '/simple/quick/')
+
+class SimilaritySearch(Resource):
+    def get(self, type1, id1, type2, by_type):
+        """
+        Expand out from a given node to another node type optionally along a particular predicate
+        ---
+        tags: [simple]
+        parameters:
+          - in: path
+            name: type1
+            description: "type of query node"
+            type: string
+            required: true
+            default: "disease"
+          - in: path
+            name: id1
+            description: "curie of query node"
+            type: string
+            required: true
+            default: "MONDO:0005737"
+          - in: path
+            name: type2
+            description: "type of return nodes"
+            type: string
+            required: true
+            default: "disease"
+          - in: path
+            name: by_type
+            description: "type used to evaluate similarity"
+            type: string
+            required: true
+            default: "phenotypic_feature"
+          - in: query
+            name: threshhold
+            description: "Number between 0 and 1 indicating the minimum similarity to return"
+            type: float
+            default: 0.5
+          - in: query
+            name: maxresults
+            description: "The maximum number of results to return. Set to 0 to return all results."
+            type: integer
+            default: 100
+          - in: query
+            name: csv
+            type: boolean
+            default: true
+          - in: query
+            name: rebuild
+            description: "Rebuild local knowledge graph for this similarity search"
+            type: boolean
+            default: false
+        responses:
+            200:
+                description: answers
+                type: object
+                properties:
+                    answers:
+                        type: array
+                        items:
+                            $ref: '#/definitions/Answer'
+        """
+        #TODO:Add another argument:
+        #- in: query
+        #  name: descendents
+        #  description: "Include ontological descendents in the result"
+        #  type: boolean
+        #  default: false
+        response = requests.post( f'http://{os.environ["BUILDER_HOST"]}:{os.environ["BUILDER_PORT"]}/api/synonymize/{id1}/{type1}/' )
+        sid1 = response.json()['id']
+        rebuild = request.args.get('rebuild', default = 'False')
+        if rebuild.upper()=='TRUE':
+            try:
+                question = {
+                    'machine_question': {
+                        'nodes': [
+                            {
+                                'id': 0,
+                                'curie': sid1,
+                                'type': type1
+                            },
+                            {
+                                'id': 1,
+                                'type': by_type
+                            },
+                            {
+                                'id': 2,
+                                'type': type2
+                            },
+                            {
+                                'id': 3,
+                                'type': by_type
+                            }
+                        ],
+                        'edges': [
+                            {
+                                'source_id': 0,
+                                'target_id': 1
+                            },
+                            {
+                                'source_id': 1,
+                                'target_id': 2
+                            },
+                            {
+                                'source_id': 2,
+                                'target_id': 3
+                            },
+                        ]
+                    }
+                }
+                response = requests.post( f'http://{os.environ["BUILDER_HOST"]}:{os.environ["BUILDER_PORT"]}/api/', json=question)
+                polling_url = f"http://{os.environ['BUILDER_HOST']}:{os.environ['BUILDER_PORT']}/api/task/{response.json()['task id']}"
+
+                for _ in range(60 * 60):  # wait up to 1 hour
+                    time.sleep(1)
+                    response = requests.get(polling_url)
+                    if response.status_code == 200:
+                        if response.json()['status'] == 'FAILURE':
+                            raise RuntimeError('Builder failed.')
+                        if response.json()['status'] == 'REVOKED':
+                            raise RuntimeError('Task terminated by admin.')
+                        if response.json()['status'] == 'SUCCESS':
+                            break
+                else:
+                    raise RuntimeError("Knowledge source querying has not completed after 1 hour. You may wish to try again later.")
+
+                logger.info('Rebuild completed, status', response.json()['status'])
+            except Exception as e:
+                logger.error(e)
+        else:
+            logger.info("No rebuild")
+
+        #Now we're ready to calculate sim
+
+        sim_params = {'threshhold':request.args.get('threshhold', default = None),
+                      'maxresults':request.args.get('maxresults', default = None)}
+        sim_params = {k:v for k,v in sim_params.items() if v is not None}
+        response = requests.get( f'http://{os.environ["RANKER_HOST"]}:{os.environ["RANKER_PORT"]}/api/similarity/{type1}/{sid1}/{type2}/{by_type}', params=sim_params)
+
+        return response.json()
+
+api.add_resource(SimilaritySearch, '/simple/similarity/<type1>/<id1>/<type2>/<by_type>')
