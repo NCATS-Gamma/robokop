7a012a7970e406d4b45c27fd21e3372e700db6fe
Sudeep Mandal <sudeep@covar.com>
2018-09-26 11:54:49 -0400

    Utilze new `mode` parameter for production/development in webpack config

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,15 +7,16 @@ const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = merge(common, {
   devtool: 'source-map',
-  plugins: [
-    new UglifyJsPlugin({
-      sourceMap: true,
-    }),
-    new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify('production'),
-    }),
-    // Note: This actually sets an environmental variable that can be used by node
-    // when packaging. Also note that process.env.NODE_ENV is not === 'production'
-    // at this point in the script (happens after execution)
-  ],
+  mode: 'production',
+  // plugins: [
+  //   new UglifyJsPlugin({
+  //     sourceMap: true,
+  //   }),
+  //   new webpack.DefinePlugin({
+  //     'process.env.NODE_ENV': JSON.stringify('production'),
+  //   }),
+  //   // Note: This actually sets an environmental variable that can be used by node
+  //   // when packaging. Also note that process.env.NODE_ENV is not === 'production'
+  //   // at this point in the script (happens after execution)
+  // ],
 });
