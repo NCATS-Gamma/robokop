f6e1f2632991befb30275ab22997b9e1477e5bb7
Kenneth Morton <kennethmorton@gmail.com>
2018-11-21 09:33:29 -0500

    Env file clean up and updates

--- deploy/manager_answer_worker.sh
+++ deploy/manager_answer_worker.sh
@@ -2,4 +2,4 @@
 
 cd $ROBOKOP_HOME/robokop
 
-celery -A manager.tasks.celery worker --loglevel=debug -c $NUM_RANKERS -n manager_answerer@robokop -Q manager_answer
+celery -A manager.tasks.celery worker --loglevel=debug -c $MANAGER_NUM_ANSWER_WORKERS -n manager_answerer@robokop -Q manager_answer
--- deploy/manager_update_worker.sh
+++ deploy/manager_update_worker.sh
@@ -2,4 +2,4 @@
 
 cd $ROBOKOP_HOME/robokop
 
-celery -A manager.tasks.celery worker --loglevel=debug -c $NUM_BUILDERS -n manager_updater@robokop -Q manager_update
+celery -A manager.tasks.celery worker --loglevel=debug -c $MANAGER_NUM_BUILDER_WORKERS -n manager_updater@robokop -Q manager_update
