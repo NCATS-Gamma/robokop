25236de265351c773d471bfd2e5d0c5395e02a8d
Kenneth Morton <kennethmorton@gmail.com>
2018-11-28 16:53:11 -0500

    Experimental deployment startup script

--- deploy/startup.sh
+++ deploy/startup.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 
-screen -S robokop -X screen
-cd backend
+screen -dmS robokop && {
+  sleep 2;
+  screen -S robokop -X -p backend screen sh -c 'cd backend; docker-compose up';
+  sleep 20;
+  screen -S robokop -X -p graph screen sh -c 'cd graph; docker-compose up';
+  screen -S robokop -X -p cache screen sh -c 'cd cache; docker-compose up';
+  sleep 20;
+  screen -S robokop -X -p manager screen sh -c 'cd manager; docker-compose up';
+  screen -S robokop -X -p rank screen sh -c 'cd ../../robokop-rank/deploy; docker-compose up';
+  screen -S robokop -X -p interfaces screen sh -c 'cd ../../robokop-interfaces/deploy; docker-compose up';
+}
+sleep 10;
+screen -rd robokop
