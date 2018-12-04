cf21d9a62a5f1c33d47f36c1b5f4fb34ec23239a
Sudeep Mandal <sudeep@covar.com>
2018-12-04 15:01:23 -0500

    MessageAnswerTable displays single member set names in rows

--- src/components/answerset/MessageAnswersetTable.jsx
+++ src/components/answerset/MessageAnswersetTable.jsx
@@ -25,15 +25,24 @@ const setFilterMethod = (filter, row, column) => { // eslint-disable-line no-unu
   return String(combined.toLowerCase()).includes(filter.value.toLowerCase());
 };
 
-// Function to calculate sane column widths
-const getColumnWidth = (data, accessor, headerText) => {
+/**
+ * Function to calculate sane column widths
+ * @param {Array of objects} data Each object element in array corresponds to a row
+ * @param {Function || String} accessor Accessor to access field value for the column
+ * @param {String} headerText Header label for this column (used for max width calc)
+ * @param {Function} accessorPostProcFn If provided, can post-process output of `accessor`
+ *    on the data object of a row prior to width calc for the field. This is useful if
+ *    a custom Cell renderer is used for a column and the accessorPostProcFn logic would
+ *    mimic the custom Cell renderer logic closely to get identical length cell strings.
+ */
+const getColumnWidth = (data, accessor, headerText, accessorPostProcFn = x => x) => {
   if (typeof accessor === 'string' || accessor instanceof String) {
     accessor = d => d[accessor]; // eslint-disable-line no-param-reassign
   }
   const maxWidth = 600;
   const magicSpacing = 9;
   const cellLength = Math.max(
-    ...data.map(row => (`${accessor(row)}` || '').length),
+    ...data.map(row => (`${accessorPostProcFn(accessor(row))}` || '').length),
     headerText.length,
   );
   return Math.min(maxWidth, cellLength * magicSpacing);
@@ -80,13 +89,32 @@ class MessageAnswersetTable extends React.Component {
       if (colSpecObj.isSet) {
         colSpecObj.accessor = d => d.nodes[nodeId].setNodes;
         colSpecObj.style = { cursor: 'pointer', userSelect: 'none' };
-        colSpecObj.Cell = () => (
-          <span>
-            <span style={{ textAlign: 'center' }}>{`${entityNameDisplay(colSpecObj.type)} [Set]`}</span>
-            <span className="pull-right">&#x2295;</span>
-          </span>
+        // cellTextFn returns array of strings to be used for display
+        // in custom Cell renderer. This modularity is so that it can
+        // be re-used in the getColumnWidth() method
+        const cellTextFn = (setNodes) => {
+          if (setNodes.length === 1) {
+            return setNodes[0].name ? [setNodes[0].name] : [setNodes[0].id];
+          }
+          return [entityNameDisplay(colSpecObj.type), `[${setNodes.length}]`];
+        };
+        colSpecObj.Cell = (row) => {
+          const cellText = cellTextFn(row.value);
+          return (
+            <span>
+              <span style={{ textAlign: 'center' }}>{`${cellText[0]} `}
+                {(cellText.length > 1) &&
+                  <span style={{ fontWeight: 'bold' }}>{cellText[1]}</span>
+                }
+              </span>
+              <span className="pull-right">&#x2295;</span>
+            </span>
+          );
+        };
+        colSpecObj.width = getColumnWidth(
+          data, colSpecObj.accessor, colSpecObj.Header,
+          setNodes => `${cellTextFn(setNodes).join(' ')}   `,
         );
-        colSpecObj.width = getColumnWidth(data, () => `${entityNameDisplay(colSpecObj.type)} [Set]   `, colSpecObj.Header);
         colSpecObj.filterMethod = setFilterMethod;
       } else {
         colSpecObj.accessor = d => (d.nodes[nodeId].name ? d.nodes[nodeId].name : d.nodes[nodeId].id);
