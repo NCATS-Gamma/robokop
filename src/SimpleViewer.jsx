820e33223d352db2c425c784032857bc8968dd63
Patrick Wang <patrick@covar.com>
2019-09-20 10:45:45 -0700

    support 0.9.2 messages in /simple/view

--- src/SimpleViewer.jsx
+++ src/SimpleViewer.jsx
@@ -77,13 +77,75 @@ class SimpleViewer extends React.Component {
     const message = _.cloneDeep(object);
 
     const hasMessage = _.isObject(message);
-    const hasQGraph = hasMessage && 'question_graph' in message;
-    const hasKG = hasMessage && 'knowledge_graph' in message;
-    const hasAnswers = hasMessage && 'answers' in message && Array.isArray(message.answers) && message.answers.length > 0;
+    let hasQGraph = hasMessage && 'question_graph' in message;
+    let hasKG = hasMessage && 'knowledge_graph' in message;
+    let hasAnswers = hasMessage && 'answers' in message && Array.isArray(message.answers) && message.answers.length > 0;
 
-    const allOk = hasQGraph && hasKG && hasAnswers;
+    let allOk = hasQGraph && hasKG && hasAnswers;
 
     if (allOk) {
+      this.setState({
+        message,
+        hasMessage: true,
+        hasError: false,
+        errorMessage: '',
+        isReading: false,
+      });
+      return;
+    }
+
+    hasQGraph = hasMessage && 'query_graph' in message;
+    hasKG = hasMessage && 'knowledge_graph' in message;
+    hasAnswers = hasMessage && 'results' in message && Array.isArray(message.results) && message.results.length > 0;
+    allOk = hasQGraph && hasKG && hasAnswers;
+
+    if (allOk) {
+      message.results.map((result) => {
+        let node_bindings = {};
+        result.node_bindings.forEach((nb) => {
+          if (nb.qg_id in node_bindings) {
+            if (Array.isArray(nb.kg_id)) {
+              node_bindings[nb.qg_id].concat(nb.kg_id);
+            }
+            else {
+              node_bindings[nb.qg_id].append(nb.kg_id);
+            }
+          }
+          else {
+            if (Array.isArray(nb.kg_id)) {
+              node_bindings[nb.qg_id] = nb.kg_id;
+            }
+            else {
+              node_bindings[nb.qg_id] = [nb.kg_id];
+            }
+          }
+        });
+        result.node_bindings = node_bindings;
+        let edge_bindings = {};
+        result.edge_bindings.forEach((eb) => {
+          if (eb.qg_id in edge_bindings) {
+            if (Array.isArray(eb.kg_id)) {
+              edge_bindings[eb.qg_id].concat(eb.kg_id);
+            }
+            else {
+              edge_bindings[eb.qg_id].push(eb.kg_id);
+            }
+          }
+          else {
+            if (Array.isArray(eb.kg_id)) {
+              edge_bindings[eb.qg_id] = eb.kg_id;
+            }
+            else {
+              edge_bindings[eb.qg_id] = [eb.kg_id];
+            }
+          }
+        });
+        result.edge_bindings = edge_bindings;
+        return result
+      });
+      message.question_graph = message.query_graph;
+      message.answers = message.results;
+
       this.setState({
         message,
         hasMessage: true,
