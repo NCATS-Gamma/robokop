32c35acc97a6129a970a4777ff5117ba1f95862e
YaphetKG <kebedey@renci.org>
2020-04-08 14:38:06 -0400

    No-ssl support for nginx proxy; New image for nginx proxy containing templates.

--- deploy/proxy/startup.sh
+++ deploy/proxy/startup.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
+template=/etc/nginx/templates/default.conf.template
+if [ "$1" == "no-ssl" ]; then
+    echo "RUNNING NO SLL"
+    template=/etc/nginx/templates/default-no-ssl.conf.template
+fi
 
 env_vars=$(awk 'BEGIN {for(v in ENVIRON) printf "$%s", v }') 
 echo "Creating default.conf file from template"
 # avoid substituting variables not defined in env. Had problem with some nginx variables being replaced.
 echo $env_vars
-envsubst $env_vars < /etc/nginx/templates/default.conf.template > /etc/nginx/conf.d/default.conf
+envsubst $env_vars < ${template} > /etc/nginx/conf.d/default.conf
 
 echo "Starting nginx daemon..."
 exec nginx -g 'daemon off;'
