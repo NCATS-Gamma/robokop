13df16f1b49ba26d331e1d90e3f81dd699bad283
Max Wang <max@covar.com>
2020-08-06 15:30:44 -0400

    adding a few simple pages to the routes and fixing the footer css

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   BrowserRouter as Router,
   Switch,
@@ -8,10 +8,9 @@ import {
 import Landing from './simplePages/Landing';
 import About from './simplePages/About';
 import Help from './simplePages/Help';
-// import Guide from './simplePages/Guide';
-// import TermsofService from './simplePages/TermsofService';
-// import MultiSearch from './simplePages/MultiSearch';
-// import SimpleViewer from './simplePages/SimpleViewer';
+import Guide from './simplePages/Guide';
+import TermsofService from './simplePages/TermsofService';
+import SimpleViewer from './simplePages/SimpleViewer';
 // import SimpleEnriched from './simplePages/SimpleEnriched';
 // import SimpleSimilarity from './simplePages/SimpleSimilarity';
 // import SimpleExpand from './simplePages/SimpleExpand';
@@ -24,51 +23,61 @@ import Help from './simplePages/Help';
 // import Question from './pages/question/Question';
 // import QuestionList from './pages/questionList/QuestionList';
 // import Answerset from './pages/answers/Answerset';
-// import Activity from './pages/activity/Activity';
 
 import Header from './components/header/Header';
 import Footer from './components/footer/Footer';
+import ensureUser from './utils/ensureUser';
 
 export default function App() {
+  const [user, setUser] = useState({});
+  const ensuredUser = ensureUser(user);
   return (
     <Router>
-      <Header user={{}} />
-      <Switch>
-        <Route path="/about">
-          <About />
-        </Route>
-        <Route path="/help">
-          <Help />
-        </Route>
-        <Route path="/guide">
-          <About />
-        </Route>
-        <Route path="/alpha">
-          <About />
-        </Route>
-        <Route path="/apps">
-          <About />
-        </Route>
-        <Route path="/termsofservice">
-          <About />
-        </Route>
-        <Route path="/activity">
-          <About />
-        </Route>
-        <Route path="/search">
-          <About />
-        </Route>
-        <Route path="/simple">
-          <About />
-        </Route>
-        <Route path="/compare">
-          <About />
-        </Route>
-        <Route path="/">
-          <Landing />
-        </Route>
-      </Switch>
-      <Footer />
+      <div id="pageContainer">
+        <Header user={ensuredUser} />
+        <div id="contentContainer">
+          <Switch>
+            <Route path="/about">
+              <About />
+            </Route>
+            <Route path="/help">
+              <Help />
+            </Route>
+            <Route path="/guide">
+              <Guide user={ensuredUser} />
+            </Route>
+            <Route path="/alpha">
+              <About />
+            </Route>
+            <Route path="/search">
+              <About />
+            </Route>
+            <Route
+              path="/simple"
+              render={({ match: { url } }) => (
+                <>
+                  <Route path={`${url}/view`} component={() => SimpleViewer({ user: ensuredUser })} exact />
+                </>
+              )}
+            />
+            {/* <Route path="/simple">
+              <Route path="/view">
+                <SimpleViewer user={ensuredUser} />
+              </Route>
+            </Route> */}
+            <Route path="/compare">
+              <About />
+            </Route>
+            <Route path="/termsofservice">
+              <TermsofService />
+            </Route>
+            <Route path="/">
+              <Landing />
+            </Route>
+          </Switch>
+        </div>
+        <Footer />
+      </div>
     </Router>
   );
 }
