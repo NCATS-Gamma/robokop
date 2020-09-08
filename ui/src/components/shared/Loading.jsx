38a59918d59b2a46384b8820a91a7d5b5d6ca5c0
Max Wang <max@covar.com>
2020-09-08 16:00:00 -0400

    playing around with loading css animations

--- ui/src/components/shared/Loading.jsx
+++ ui/src/components/shared/Loading.jsx
@@ -3,11 +3,12 @@ import { BubbleLoader } from 'react-css-loaders';
 
 const Loading = (props) => {
   const showMessage = Boolean(props && ('message' in props));
+  const { message } = props;
 
   return (
     <div className="center-block">
       <BubbleLoader color="#b8c6db" />
-      {showMessage && props.message}
+      {showMessage && message}
     </div>
   );
 };
