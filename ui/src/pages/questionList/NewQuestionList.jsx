175fce018ccc174f155246d89b404afb3521ccd3
Max Wang <max@covar.com>
2020-09-16 12:39:20 -0400

    adding check for question list if there are no questions

--- ui/src/pages/questionList/NewQuestionList.jsx
+++ ui/src/pages/questionList/NewQuestionList.jsx
@@ -30,8 +30,11 @@ export default function NewQuestionList({ user }) {
         },
       })
         .then((res) => {
-          // TODO: there might be a new route that will get just questions
-          const fetchedQuestions = res.data.filter((question) => !question.parent);
+          let fetchedQuestions = [];
+          if (Array.isArray(res.data)) {
+            // TODO: there might be a new route that will get just questions
+            fetchedQuestions = res.data.filter((question) => !question.parent);
+          }
           updateQuestions(fetchedQuestions);
           toggleLoading(false);
         })
